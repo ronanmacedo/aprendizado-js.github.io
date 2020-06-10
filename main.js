19df2fa82d733e831205afb881c1d9706b025181
Ronan Mega <ronan.macedo.edu@gmail.com>
2020-06-10 16:44:40 -0300

    Interface da primeira página.

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * JavaScript do aprendizado-js
+ */
+var linkElementUno = document.createElement('a');
+linkElementUno.setAttribute('href', 'moduloUno/index.html');
+
+var textElementUno = document.createTextNode("Acessar primeiro módulo.");
+linkElementUno.appendChild(textElementUno);
+
+var divUno = document.querySelector('#linkUno');
+divUno.appendChild(linkElementUno);
