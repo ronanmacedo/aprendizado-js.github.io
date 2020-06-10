0298001a88d57f43443b8a8889082cd27c6ae832
Ronan Mega <ronan.macedo.edu@gmail.com>
2020-06-10 16:51:27 -0300

    Correção da interface.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
  * JavaScript do aprendizado-js
  */
-// var linkElementUno = document.createElement('a');
-// linkElementUno.setAttribute('href', 'moduloUno/index.html');
+var linkElementUno = document.createElement('a');
+linkElementUno.setAttribute('href', 'moduloUno/index.html');
 
-// var textElementUno = document.createTextNode("Acessar primeiro módulo.");
-// linkElementUno.appendChild(textElementUno);
+var textElementUno = document.createTextNode("Acessar primeiro módulo.");
+linkElementUno.appendChild(textElementUno);
 
-// var divUno = document.querySelector('#linkUno');
-// divUno.appendChild(linkElementUno);
+var divUno = document.querySelector('#linkUno');
+divUno.appendChild(linkElementUno);
